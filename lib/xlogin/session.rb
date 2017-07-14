12baf98ce673a9f30ce40f37d4930b4cdaa53e2f
haccht <haccht@users.noreply.github.com>
2017-07-14 09:50:14 +0900

    Fix typo

--- lib/xlogin/session.rb
+++ lib/xlogin/session.rb
@@ -36,7 +36,7 @@ module Xlogin
         begin
           safe_session ||= Xlogin.get(@host, @opts)
           yield safe_session
-        rescue Errno::ECONNRESET, Errorno::EPIPE => e
+        rescue Errno::ECONNRESET, Errno::EPIPE => e
           raise e unless (retry_count += 1) < max_retry
           safe_session = nil
           retry
