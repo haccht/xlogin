5fc87e48e9120403e69eb23f7120b10c2aeee13c
haccht <haccht@users.noreply.github.com>
2017-09-15 17:08:34 +0900

    Add prohibited command for iosxr

--- lib/xlogin/templates/iosxr.rb
+++ lib/xlogin/templates/iosxr.rb
@@ -8,4 +8,14 @@ Xlogin.configure :iosxr do |os|
     waitfor(/Password: /) && puts(password)
     waitfor
   end
+
+  os.hook do |command|
+    if command =~ /^(?:adm |admi |admin )?\s*(?:con|rel|red|cle|hw|ro)/
+      # commands: configure, reload, redundancy, clear, hw-module, rollback
+      #   with or without admin prefix is prihibited if not authorized.
+      raise Xlogin::AuthorizationError.new("prohibited command: #{command}") unless Xlogin.authorized?
+    end
+
+    pass(command)
+  end
 end
