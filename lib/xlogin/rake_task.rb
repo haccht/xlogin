64ca5fd84b1f93e9c23e997fd94a58e14dea12ad
haccht <haccht@users.noreply.github.com>
2018-03-13 11:25:14 +0900

    Add support for fnmatch to RakeTask

--- lib/xlogin/rake_task.rb
+++ lib/xlogin/rake_task.rb
@@ -1,5 +1,6 @@
 require 'rake'
 require 'rake/tasklib'
+require 'ostruct'
 require 'stringio'
 
 module Xlogin
@@ -9,14 +10,18 @@ module Xlogin
     class << self
       include Rake::DSL
 
-      def bulk(names, &block)
-        names = names.map(&:strip).grep(/^\s*[^#]/).uniq
-        description = Rake.application.last_description
+      def generate(*target, **opts, &block)
+        hostnames = target.flat_map { |e| Xlogin.factory.list(e) }.map { |e| e[:name] }
 
-        names.each do |name|
+        description = Rake.application.last_description
+        task 'all' => hostnames unless opts[:bundle] == false
+
+        description = opts[:desc] if opts.key?(:desc)
+        hostnames.each do |hostname|
           desc description
-          RakeTask.new(name, &block)
+          RakeTask.new(hostname, &block)
         end
+
       end
     end
 
@@ -24,66 +29,75 @@ module Xlogin
     attr_accessor :lock
     attr_accessor :log
     attr_accessor :silent
-    attr_accessor :timeout
     attr_accessor :fail_on_error
 
     def initialize(name)
       @name   = name
       @runner = nil
+      @config = OpenStruct.new
       @silent ||= Rake.application.options.silent
       @fail_on_error = true
 
-      yield(self) if block_given?
-      define_task
+      yield self if block_given?
+      define
     end
 
-    def start(&block)
+    def run(&block)
       @runner = block
     end
 
+    def method_missing(name, *args, &block)
+      super(name, *args, &block) unless name.to_s =~ /^\w+=$/
+      @config.send(name, *args)
+    end
+
     private
-    def define_task
+    def define
       description = Rake.application.last_description
       description = "#{description} - #{name}" if description
       desc description
 
+      mkdir_p(File.dirname(log),  verbose: Rake.application.options.trace) if log
+      mkdir_p(File.dirname(lock), verbose: Rake.application.options.trace) if lock
+
       if lock
         task(name => lock)
         file(lock) do
-          invoke
-          mkdir_p(File.dirname(lock), verbose: Rake.application.options.trace)
-          touch(lock, verbose: Rake.application.options.trace)
+          File.open(lock, 'w') do |fd|
+            run_task if fd.flock(File::LOCK_EX|File::LOCK_NB)
+          end
         end
       else
-        task(name) { invoke }
+        task(name) do
+          run_task
+        end
       end
     end
 
-    def invoke
+    def run_task
       buffer  = StringIO.new
       loggers = []
-      loggers << buffer  if Rake.application.options.always_multitask
-      loggers << $stdout unless Rake.application.options.always_multitask || silent
-
-      if log
-        mkdir_p(File.dirname(log), verbose: Rake.application.options.trace)
-        loggers << log
-      end
+      loggers << log     if log
+      loggers << buffer  if !silent &&  Rake.application.options.always_multitask
+      loggers << $stdout if !silent && !Rake.application.options.always_multitask
 
       begin
-        session = Xlogin.get(name, log: loggers, timeout: timeout)
-
-        @runner.call(session)
+        session = Xlogin.get(name, log: loggers, **@config.to_h)
+        session.instance_eval(&@runner)
         session.close if session
+
+        output($stdout, buffer.string) if !silent && Rake.application.options.always_multitask
       rescue => e
-        $stderr.print "[ERROR] #{name} - #{e}\n"
+        output($stderr, buffer.string) if !silent && Rake.application.options.always_multitask
+        output($stderr, "[ERROR] Xlogin - #{e}\n")
         raise e if fail_on_error
       end
+    end
 
-      if Rake.application.options.always_multitask && !silent
-        lines = buffer.string.lines.map { |line| "#{name}\t" + line.gsub("\r", '') }
-        lines.each { |line| $stdout.print "#{line.chomp}\n" }
-      end
+    def output(fp, text)
+      prefix = (Rake.application.options.always_multitask)? "#{name}\t| " : ""
+      lines  = text.lines.map { |line| "#{prefix}#{line.strip}\n" }
+      lines.each { |line| $stdout.print line }
     end
 
   end
